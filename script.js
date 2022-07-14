b9214f655986b72d2edc241d5638b06d9ebd9ec8
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-14 19:01:46 -0300

    validacion del tel

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-// 3) Validar que los campos no esten vacios
-// 4) Validar que el mail tenga un @ y un . (y que primero este el arroba)
-// 5) Validar que telefono sea 100% numerico
-// 6) Validar que mensaje tenga menos de 280 caracteres 
-/* <section>
-<form action="" method="post" class="formulario" id="form"></form>
-<input type="text" id="user-name">
-<label for="user-name">ingrese su usuario</label>
-<input type="email" id="user-email">
-<label for="user-email">ingrese su email</label>
-<input type="number" id="user-number">
-<label for="user-number">ingrese su numero</label>
-<textarea name="user-msg" id=",esg" cols="30" rows="10" placeholder="escriba"></textarea>
-<input type="submit" value="enviar" class="enviar"> */
+document.getElementById("contacto_usuario").addEventListener("submit", validarFormulario);
 
-document.getElementById("form").addEventListener('submit',validarFormulario)
+function validarFormulario(evento) {
+  evento.preventDefault();
+  let nombre = document.getElementById("nombre").value;
+  let mail = document.getElementById("mail").value;
+  let telefono = document.getElementById("telefono").value;
+  let mensaje = document.getElementById("mensaje").value;
+  // nombre.length == 0  o nombre == ""
+  if (nombre == "" || mail == "" || telefono == "" || mensaje == "") {
+    alert("Completa todos los datos");
+    return;
+  }
 
-function validarFormulario (evento){
-    evento.preventDefault();
-    let nombre = document.getElementById("user-name").value;
-    let mail = document.getElementById("user-email").value;
-    let telefono = document.getElementById("user-number").value;
-    let mensaje = document.getElementById("mensage").value;
-
-// valido que complete todo 
-    if (nombre = "" || mail == "" || telefono == "" || mensaje == "") {
-        alert("Porfavor, complete todo el formulario")
-        return
-    }
   // valido el mail
   let posicionArroba = mail.indexOf("@");
   let posicionPunto = mail.lastIndexOf(".");
